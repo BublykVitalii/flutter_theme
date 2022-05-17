0ca2b60d67570ebdafcd4de7b206783db2217beb
John Doe <sk8vetal@gmail.com>
2022-05-17 17:33:15 +0300

    add ui code

--- lib/application.dart
+++ lib/application.dart
@@ -1,55 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_theme/infrastructure/app_images.dart';
 import 'package:flutter_theme/infrastructure/theme/app_colors.dart';
+import 'package:flutter_theme/infrastructure/theme/theme_extensions.dart';
+import 'package:flutter_theme/ui_screen.dart';
 
-class Application extends StatefulWidget {
-  const Application({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<Application> createState() => _ApplicationState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _ApplicationState extends State<Application> {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const NeumorphicDesign();
-  }
-}
-
-class NeumorphicDesign extends StatelessWidget {
-  const NeumorphicDesign({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: AppColors.background,
-        appBar: AppBar(
-          automaticallyImplyLeading: true,
-          title: const Text('Theme'),
-          backgroundColor: AppColors.background,
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: AppColors.accent,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black,
-                  offset: Offset(1, 1),
-                  blurRadius: 0.1,
-                  spreadRadius: 0.1,
-                ),
-              ],
-            ),
-            child: Image.asset(AppImages.logo),
-          ),
-        ),
+    final theme = ThemeData(
+      colorScheme: context.theme.colorScheme.copyWith(
+        primary: AppColors.black,
       ),
     );
+
+    return MaterialApp(
+      onGenerateRoute: onGenerateRoute,
+      theme: theme,
+    );
+  }
+
+  Route onGenerateRoute(RouteSettings? settings) {
+    return UiScreen.route;
   }
 }
